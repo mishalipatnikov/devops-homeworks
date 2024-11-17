2ef14eb5c7b277e2aa1b5253c4642c8dd0b8724d
mishalipatnikov <misha-lipatnikov@mail.ru>
2024-11-17 20:56:49 +0300

    ansible-03

---
+++ ansible-03/terraform/main.tf
@@ -0,0 +1,44 @@
+resource "yandex_vpc_network" "develop" {
+  name = var.vpc_name
+}
+resource "yandex_vpc_subnet" "develop" {
+  name           = var.vpc_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
+}
+
+
+data "yandex_compute_image" "ubuntu" {
+  family = var.vm_web_image
+}
+resource "yandex_compute_instance" "platform" {
+  count = 3
+  name          = "${local.vm_names.web_vm_name}-${count.index + 1}"
+  platform_id   = "standard-v3"
+  resources {
+  cores         = var.vm_resources.cores
+  memory        = var.vm_resources.memory
+  core_fraction = var.vm_resources.core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
+
+}
+
